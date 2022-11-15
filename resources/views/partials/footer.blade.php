99991d3fbcbb10e30531b5e956ebeaac471d434c
Jiří Jehlář <33741898+Herman1994@users.noreply.github.com>
2022-11-15 20:52:00 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -54,16 +54,6 @@
                 <li><a href="{{ route('about') }}">{{ __('common.about') }}</a></li>
             </ul>
         </div>
-
-        <div class="col-md-2 l-footer-section">
-            <h2 class="l-footer-section-title">{{ __('common.other') }}</h2>
-            <ul>
-                <li><a href="https://github.com/sponsors/HDVinnie" target="_blank"
-                       class="btn btn-xs btn-primary">{{ __('common.sponsor') }}</a></li>
-                <li><a href="https://github.com/HDInnovations/UNIT3D" target="_blank"
-                       class="btn btn-xs btn-primary">{{ __('common.powered-by') }}</a></li>
-            </ul>
-        </div>
     </div>
 </div>
 
